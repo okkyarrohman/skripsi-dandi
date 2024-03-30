2f2968ab37c24f15ca139c3e4860bf031684ccc1
Muhammad Ainurrofiq Anwar Buhang <87835637+RafiArt@users.noreply.github.com>
2024-03-30 11:53:38 +0700

    lagii

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -30,17 +30,12 @@
     <!-- Icons -->
     <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
     <link rel="stylesheet" href="../../assets/vendor/fonts/tabler-icons.css" />
-    <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
 
     <!-- Core CSS -->
     <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" />
     <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" />
     <link rel="stylesheet" href="../../assets/css/demo.css" />
 
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
-    <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-    <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
     <!-- Vendor -->
     <link rel="stylesheet" href="../../assets/vendor/libs/@form-validation/form-validation.css" />
 
@@ -66,14 +61,7 @@
     <!-- build:js assets/vendor/js/core.js -->
 
     <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../../assets/vendor/libs/popper/popper.js"></script>
     <script src="../../assets/vendor/js/bootstrap.js"></script>
-    <script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
-    <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
-    <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
-    <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
-    <script src="../../assets/vendor/js/menu.js"></script>
 
     <!-- endbuild -->
 
@@ -85,7 +73,5 @@
     <!-- Main JS -->
     <script src="../../assets/js/main.js"></script>
 
-    <!-- Page JS -->
-    <script src="../../assets/js/pages-auth.js"></script>
   </body>
 </html>
